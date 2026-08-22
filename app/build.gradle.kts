a1a063f18904e961f5502483a8014fffd8984043
Alin-J178 <fadlhlin.j178204@gmail.com>
2026-08-23 01:00:35 +0800

    just fix version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.fileconverter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "3.2"
+        versionCode = 24
+        versionName = "3.2.0"
     }
 
     buildTypes {
